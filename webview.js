402bc3100f1c3d93f051e15e3da73054d2ef568d
Steve DaSilva <sdasilva@angelogordon.com>
2019-03-01 12:26:46 -0500

    convert ciscospark recipe to webexteams

---
+++ webview.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+module.exports = (Franz) => {
+  const UNREAD_BADGE_SELECTOR = '.navigation-list-item--badgeCount';
+
+  const getMessages = function getMessages() {
+    let directCount = 0;
+    let indirectCount = 0;
+
+    const elements = document.querySelectorAll('.navigation-list .navigation-list-item');
+    if (elements.length > 0 && elements[1].querySelector(UNREAD_BADGE_SELECTOR)) {
+      directCount = parseInt(elements[1].querySelector(UNREAD_BADGE_SELECTOR).textContent, 10);
+    }
+    if (elements.length > 0 && elements[2].querySelector(UNREAD_BADGE_SELECTOR)) {
+      indirectCount = parseInt(elements[2].querySelector(UNREAD_BADGE_SELECTOR).textContent, 10);
+    }
+
+    Franz.setBadge(directCount, indirectCount);
+  };
+
+  Franz.loop(getMessages);
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+};
